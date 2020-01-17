b20a439cc50ee5b02bef96fe6a06ae0803cb2f53
wardahmad <whitelight43@hotmail.com>
2020-01-17 23:01:14 +0300

    Updates

---
+++ precache-manifest.309f8e6359a177ae88a7cac221a0b939.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae70e12e2cdc0c2ee54d91b2ceba44a",
+    "url": "/jeddahGuide/index.html"
+  },
+  {
+    "revision": "525df803d24cacfa8243",
+    "url": "/jeddahGuide/static/css/2.712a3911.chunk.css"
+  },
+  {
+    "revision": "9bd58aad6fb8c7f9e61b",
+    "url": "/jeddahGuide/static/css/main.e8d7b7fc.chunk.css"
+  },
+  {
+    "revision": "525df803d24cacfa8243",
+    "url": "/jeddahGuide/static/js/2.d5654b03.chunk.js"
+  },
+  {
+    "revision": "d342664d578042145f5010e743958e89",
+    "url": "/jeddahGuide/static/js/2.d5654b03.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9bd58aad6fb8c7f9e61b",
+    "url": "/jeddahGuide/static/js/main.7bdde8fb.chunk.js"
+  },
+  {
+    "revision": "4471f2a806f46330d46e",
+    "url": "/jeddahGuide/static/js/runtime-main.d91de658.js"
+  }
+]);
